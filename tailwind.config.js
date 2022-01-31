cf82059c9568e765a18da4d8c59eeaf1a2a510e8
Naomi Kangwa <naomi@yawt.digital>
2022-01-31 16:35:11 +0000

    adding basic nav and button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,55 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: 'Lato'
+      },
+      colors: {
+        'morning-blue': '#8aa399',
+        rhythm: '#7d84b2',
+        'wild-blue-yonder': '#8fa6cb',
+        'yellow-green-crayola': '#dbf4a7',
+        'aero-blue': '#d5f9de'
+      },
+      animation: {
+        'gradient-x':'gradient-x 10s ease infinite',
+        'gradient-y':'gradient-y 15s ease infinite',
+        'gradient-xy':'gradient-xy 15s ease infinite',
+      },
+      keyframes: {
+        'gradient-y': {
+          '0%, 100%': {
+              'background-size':'400% 400%',
+              'background-position': 'center top'
+          },
+          '50%': {
+              'background-size':'200% 200%',
+              'background-position': 'center center'
+          }
+        },
+        'gradient-x': {
+          '0%, 100%': {
+              'background-size':'200% 200%',
+              'background-position': 'left center'
+          },
+          '50%': {
+              'background-size':'200% 200%',
+              'background-position': 'right center'
+          }
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+              'background-size':'400% 400%',
+              'background-position': 'left center'
+          },
+          '50%': {
+              'background-size':'200% 200%',
+              'background-position': 'right center'
+          }
+      }
+      }
+    },
   },
   plugins: [],
 }
